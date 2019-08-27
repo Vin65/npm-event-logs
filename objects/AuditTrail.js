9b70bc8ff8e63a0ebc9c34dac726eb3dd7da3059
Raman Buttar <raman.buttar@winedirect.com>
2019-08-27 14:22:20 -0700

    add audit trail object

---
+++ objects/AuditTrail.js
@@ -0,0 +1,43 @@
+'use strict';
+
+const httpRequest = require('request');
+
+class AuditTrail {
+  constructor(data) {
+    this.data = data;
+  }
+  
+  static _headers() {
+    return {
+      'Content-Type': 'application/json',
+      Authorization: process.env.EVENT_LOGS_TOKEN
+    };
+  }
+  
+  _body() {
+    return {
+      data: this.data
+    };
+  }
+  
+  create(shouldPost) {
+    let options = {
+        url:      `${process.env.EVENT_LOGS_URL}auditTrail`,
+        headers:  this.constructor._headers(),
+        body:     JSON.stringify(this._body())
+    };
+    if (shouldPost) {
+      httpRequest.post(options, (error, response, body) => {
+        // process returned response
+        if (error) throw new Error(error);
+      }).on('error', (error) => {
+        console.error(error);
+      });
+    } else {
+      console.info('Logging AuditTrail:');
+      console.dir(this._body());
+    }
+  }
+}
+
+module.exports = AuditTrail;
